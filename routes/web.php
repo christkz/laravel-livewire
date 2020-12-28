0f7a1463ea8e0561b74287f3ba15fe23af238fd1
kezakimana <kezakimanachrist2@gmail.com>
2020-12-28 13:57:49 +0200

    registre filiere

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\DashboardLivewire;
+use App\Http\Livewire\Filiere;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,3 +20,4 @@ use App\Http\Livewire\DashboardLivewire;
 // });
 
 Route::get('/', DashboardLivewire::class);
+Route::get('filieres',Filiere::class);
