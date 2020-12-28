97711129ebe2669c05374ec7e8ac4b50d3a5fad1
kezakimana <kezakimanachrist2@gmail.com>
2020-12-28 13:57:37 +0200

    add link filieres in  sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -8,7 +8,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="nav-link" href="{{url('filieres')}}">
                     <span data-feather="file"></span>
                     Filieré
                 </a>
